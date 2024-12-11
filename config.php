0f92dd5087ae2ef402d98a1b85114a57e9c2ab35
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-12-11 12:00:27 +0530

    Update config.php

--- config.php
+++ config.php
@@ -24,6 +24,22 @@ class Config
         $query = "INSERT INTO students (name, age, contact, course) VALUES ('$name', '$age', '$contact', '$course')";
         mysqli_query($this->connection, $query);
     }
+
+    public function readData(){
+        $query = "SELECT * FROM $this->tableName";
+        $res = mysqli_query($this->connection, $query);
+        return $res;
+    }
+
+    public function updateData($id, $name, $age, $contact, $course){
+        $query = "UPDATE $this->tableName SET name = '$name', age = $age, contact = $contact, course = '$course' WHERE id = $id";
+        return $res = mysqli_query($this->connection, $query);
+    }
+
+    public function deletedata($id){
+        $query = "DELETE FROM $this->tableName WHERE id = $id";
+        return $res = mysqli_query($this->connection, $query);
+    }
 }
 
 ?>
